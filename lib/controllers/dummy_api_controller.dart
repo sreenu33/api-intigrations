81d65244b22e0224b98d3193adba19d4e79177a0
sreenu33 <sreenivaskareti33@gmail.com>
2026-01-29 12:34:01 +0530

    cart products are added

---
+++ lib/controllers/dummy_api_controller.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:api_integrations/models/user_model.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+class DummyApiController extends GetxController {
+   List<UserModel> usersData = [];
+  final String api =
+      "https://fakestoreapi.com/carts";
+  Future<void> getAllUsers() async {
+    final responce = await http.get(Uri.parse(api));
+    log("responce>>>>>${responce.body}");
+    if (responce.statusCode == 200) {
+      final List data = jsonDecode(responce.body);
+       usersData = data.map((json) => UserModel.fromJson(json)).toList();
+      update();
+    } else {
+      throw Exception("Failed to load Users");
+    }
+  }
+}
